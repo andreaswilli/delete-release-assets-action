205e6fb3854c2ecba196b18c4b8ef759d9eac055
Andreas Willi <andiwilli8460@gmail.com>
2021-10-09 22:14:13 +0200

    Implement action

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import fs from "fs/promises";
+import core from "@actions/core";
+import github from "@actions/github";
+import { Octokit } from "@octokit/rest";
+
+try {
+  main();
+} catch (e) {
+  core.setFailed(e.message);
+}
+
+async function main() {
+  const tag = core.getInput("tag") || (await getVersionFromPackageJson());
+  const githubToken = core.getInput("github_token");
+  const { repo, owner } = github.context.repo;
+
+  if (!tag) {
+    core.setFailed(
+      "Missing value for 'tag'. Was not specified and failed to read from package.json"
+    );
+  }
+
+  if (!githubToken) {
+    core.setFailed(
+      "GitHub Access Token was not specified but is required for this action!"
+    );
+  }
+
+  const octokit = new Octokit({ auth: githubToken });
+
+  core.info("Getting releases...");
+  const { data: releases } = await octokit.request(
+    `GET /repos/{owner}/{repo}/releases`,
+    { owner, repo }
+  );
+  const release = releases.find((r) => r.tag_name === `v${tag}`);
+
+  if (!release) {
+    core.setFailed(`No release for tag v${tag} found.`);
+  }
+  core.info(`Release ${release.tag_name} found.`);
+
+  if (!release.draft) {
+    core.notice(
+      `Not deleting assets because ${release.tag_name} is not a draft.`
+    );
+    return;
+  }
+
+  core.info(`Found ${release.assets.length} assets.`);
+
+  await Promise.all(
+    release.assets.map((a) => {
+      core.info(`Deleting ${a.name}`);
+      return octokit.request(
+        "DELETE /repos/{owner}/{repo}/releases/assets/{asset_id}",
+        { owner, repo, asset_id: a.id }
+      );
+    })
+  );
+
+  core.info("Successfully deleted assets!");
+}
+
+async function getVersionFromPackageJson() {
+  core.info("Input 'tag' not specified. Reading version from package.json");
+  return (await fs.readFile("./package.json")).toJSON().version;
+}
